0c5b6823f2ab53f2ab48cd4b17ad63a29714db4c
afabijanska <an_fab@kis.p.lodz.pl>
2019-05-30 17:09:55 +0200

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -16,19 +16,3 @@ def load_hdf5(infile):
 def write_hdf5(arr,outfile):
   with h5py.File(outfile,"w") as f:
     f.create_dataset("image", data=arr, dtype=arr.dtype)
-
-#group a set of images row per columns
-def group_images(data,per_row):
-    assert data.shape[0]%per_row==0
-    assert (data.shape[1]==1 or data.shape[1]==3)
-    data = np.transpose(data,(0,2,3,1))  #corect format for imshow
-    all_stripe = []
-    for i in range(int(data.shape[0]/per_row)):
-        stripe = data[i*per_row]
-        for k in range(i*per_row+1, i*per_row+per_row):
-            stripe = np.concatenate((stripe,data[k]),axis=1)
-        all_stripe.append(stripe)
-    totimg = all_stripe[0]
-    for i in range(1,len(all_stripe)):
-        totimg = np.concatenate((totimg,all_stripe[i]),axis=0)
-    return totimg
